c1fd63f8be7f185548171265d9f3537ab7a9469c
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-23 10:49:58 -0300

    Read Contacts Permission

--- app/src/main/java/br/mdan/contactsproject/MainActivity.kt
+++ app/src/main/java/br/mdan/contactsproject/MainActivity.kt
@@ -1,11 +1,48 @@
 package br.mdan.contactsproject
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CONTACT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_CONTACTS),
+            REQUEST_CONTACT)
+        } else {
+            setContacts()
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CONTACT) setContacts()
+    }
+
+    private fun setContacts() {
+        TODO()
     }
 }
+
+
+
+
+
+
+
