dbdb368e64b45fe4edeafba5e45c54406bd20128
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-22 22:16:13 -0300

    ViewHolder

--- app/src/main/java/br/mdan/contactsproject/ContactsAdapter.kt
+++ app/src/main/java/br/mdan/contactsproject/ContactsAdapter.kt
@@ -1,15 +1,17 @@
 package br.mdan.contactsproject
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ContactsAdapter(val contactsList: ArrayList<Contact>):
+class ContactsAdapter(val contactsList: ArrayList<Contact>) :
     RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            ContactsAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactsAdapter.ViewHolder, position: Int) {
@@ -18,6 +20,18 @@ class ContactsAdapter(val contactsList: ArrayList<Contact>):
 
     override fun getItemCount(): Int {
         return contactsList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindItem(contact: Contact) {
+
+            val nome = itemView.findViewById<TextView>(R.id.tvName)
+            val fone = itemView.findViewById<TextView>(R.id.tvPhone)
+
+            nome.text = contact.name
+            fone.text = contact.phone
+        }
     }
 
 }
