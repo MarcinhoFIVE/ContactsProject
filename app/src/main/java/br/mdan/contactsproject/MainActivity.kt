0403c18a18693f04bbe38e049fb77f61fefe8690
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-23 11:54:44 -0300

    Concluded

--- app/src/main/java/br/mdan/contactsproject/MainActivity.kt
+++ app/src/main/java/br/mdan/contactsproject/MainActivity.kt
@@ -5,11 +5,15 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     val REQUEST_CONTACT = 1
+    val LINEAR_LAYOUT_VERTICAL = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +40,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setContacts() {
-        TODO()
+        val contactList: ArrayList<Contact> = ArrayList()
+
+        val cursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+        null,
+        null,
+        null,
+        null)
+        
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                contactList.add(Contact(
+                    //cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(
+                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    ))
+            }
+            cursor.close()
+        }
+
+        val adapter = ContactsAdapter(contactList)
+        val contactRecyclerView = findViewById<RecyclerView>(R.id.rvContacts)
+
+        contactRecyclerView.layoutManager = LinearLayoutManager(this,
+            LINEAR_LAYOUT_VERTICAL,
+            false)
+        contactRecyclerView.adapter = adapter
     }
 }
 
